5445cedad2d9b63046342af5c1eed0ac6692f61c
Rin Okuyama <rin@iij.ad.jp>
2023-08-29 14:32:02 +0900

    amd64, i386: XXX: Workaround gcc12 for x86_curlwp()

--- sys/arch/i386/include/cpu.h
+++ sys/arch/i386/include/cpu.h
@@ -70,10 +70,19 @@ x86_curlwp(void)
 {
 	lwp_t *l;
 
+/*
+ * XXXGCC12.
+ */
+#pragma GCC push_options
+#pragma GCC diagnostic ignored "-Warray-bounds"
+
 	__asm("movl %%fs:%1, %0" :
 	    "=r" (l) :
 	    "m"
 	    (*(struct cpu_info * const *)offsetof(struct cpu_info, ci_curlwp)));
+
+#pragma GCC pop_options
+
 	return l;
 }
 
