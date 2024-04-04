974ecceb30b62f994f1f592191e762159b7b2128
yamaguchi <yamaguchi@netbsd.org>
2024-04-04 08:26:32 +0000

    XXX
    lagg(4): added size check to SIOCSLAGG
    
    pointed out by ozaki-r@, thanks.

--- sys/net/lagg/if_lagg.c
+++ sys/net/lagg/if_lagg.c
@@ -693,7 +693,10 @@ lagg_ioctl(struct ifnet *ifp, u_long cmd, void *data)
 			break;
 
 		nports = laggreq.lrq_nports;
-		if (nports > 1) {
+		if (nports > LAGG_MAX_PORTS) {
+			error = ENOMEM;
+			break;
+		} else if (nports > 0) {
 			allocsiz = sizeof(struct lagg_req)
 			    + sizeof(struct laggreqport) * nports;
 			buf = kmem_alloc(allocsiz, KM_SLEEP);
