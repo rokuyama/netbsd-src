85757fa260ee4579a3ec3e3f1c8578fffa022033
Rin Okuyama <rin@iij.ad.jp>
2024-10-29 11:36:06 +0900

    openssh: Fix mlkem768x25519-sha256 KEX algorithm for big-endian machines
    This KEX algorithm has been added to OpenSSH 9.9, and no release
    branches are affected.
    
    Cherry-picked from upstream:
    
    (1/2) https://github.com/openbsd/src/commit/66207d7b19f
    fix ML-KEM768x25519 KEX on big-endian systems;
    spotted by jsg@ feedback/ok deraadt@
    
    (2/2) https://github.com/openbsd/src/commit/c42fee07eda
    explicitly include endian.h

--- crypto/external/bsd/openssh/dist/kexmlkem768x25519.c
+++ crypto/external/bsd/openssh/dist/kexmlkem768x25519.c
@@ -1,5 +1,5 @@
 /*	$NetBSD: kexmlkem768x25519.c,v 1.2 2024/09/24 21:32:18 christos Exp $	*/
-/* $OpenBSD: kexmlkem768x25519.c,v 1.1 2024/09/02 12:13:56 djm Exp $ */
+/* $OpenBSD: kexmlkem768x25519.c,v 1.2 2024/10/27 02:06:59 djm Exp $ */
 /*
  * Copyright (c) 2023 Markus Friedl.  All rights reserved.
  *
@@ -33,6 +33,7 @@ __RCSID("$NetBSD: kexmlkem768x25519.c,v 1.2 2024/09/24 21:32:18 christos Exp $")
 #include <stdbool.h>
 #include <string.h>
 #include <signal.h>
+#include <endian.h>
 
 #include "sshkey.h"
 #include "kex.h"
