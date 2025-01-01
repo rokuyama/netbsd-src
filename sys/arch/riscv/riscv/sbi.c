bfb46d101c586b4606746f306fbe3a4fb0e6db9a
Rin Okuyama <rokuyama.rk@gmail.com>
2025-01-01 09:00:26 +0900

    Revert "riscv: sbi: XXX: legacy shutdown take #2"
    This reverts commit 2d978933f544690a67e80d1b144442e110f1a3de.

--- sys/arch/riscv/riscv/sbi.c
+++ sys/arch/riscv/riscv/sbi.c
@@ -213,13 +213,16 @@ sbi_hart_suspend(uint32_t suspend_type, unsigned long resume_addr,
 struct sbiret
 sbi_system_reset(uint32_t reset_type, uint32_t reset_reason)
 {
-#if 0 // XXXRO
-	return SBI_CALL2(SBI_EID_SRST, SBI_FID_SRST_SYSTEMRESET,
+	struct sbiret ret = SBI_CALL2(SBI_EID_SRST, SBI_FID_SRST_SYSTEMRESET,
 	    reset_type, reset_reason);
-#else
+
+#if 1 // XXXRO
 #define	SBI_LEGACY_SHUTDOWN	8
-	return SBI_CALL0(0, SBI_LEGACY_SHUTDOWN);
+	if (ret.error != 0)
+		ret = SBI_CALL0(0, SBI_LEGACY_SHUTDOWN);
 #endif
+
+	return ret;
 }
 
 struct sbiret
